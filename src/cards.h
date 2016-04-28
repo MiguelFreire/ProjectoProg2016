773cad7ab0e359b6cd4bdefedfb772d88fe3abdf
MiguelFreire <mfreire12@icloud.com>
2016-04-28 18:21:24 +0100

    Added Cards, Players File and TODO list

---
+++ src/cards.h
@@ -0,0 +1,44 @@
+/**
+ * Enum type of suit
+ */
+enum Suit{club=1, diamonds, hearts, spades}
+
+/**
+ * Card is a ordinary struct
+ */
+typedef struct {
+  int rank;
+  Suit suit;
+  int value;
+} Card;
+
+/**
+ *	Deck is a linked list
+ */
+typedef struct {
+  Card *stack;
+  struct Deck *next;
+} Deck;
+
+/**
+ * Hand is a stack
+ */
+typedef struct {
+  Player owner;
+  Hand   *next;
+} Hand;
+
+/*****************
+ * Stack FUNCTION
+ ****************/
+Hand* initHand() {
+  return NULL;
+}
+
+Hand* pushCard(Hand* top, Player owner) {
+  
+}
+
+Hand* popCard() {
+
+}
