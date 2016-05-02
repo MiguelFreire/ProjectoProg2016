d014639d7ecf85df93719b4544ca8c1a1eb6047d
andrefagostinho <andre.f.agostinho@tecnico.ulisboa.pt>
2016-05-02 14:16:35 +0100

    Did part of the program flow

--- src/main.c
+++ src/main.c
@@ -19,5 +19,44 @@
 #include "cards.h"
 
 int main(int argc, char *argv[]){
+	SDL_Event event;
+	bool quit = 0;
 
+	while( !quit ){
+		while(SDL_PollEvent(&event)){
+			switch (event.type){
+
+				// check for key press
+				case SDL_KEYDOWN:
+					switch( event.key.keysym.sym){
+
+						case SLD_h: // hit
+						
+						case SLD_s: // stand
+							
+						case SLD_n: // new game
+							
+						case SLD_q: // quit
+							quit = 1
+						case SDL_d: // double
+							
+						case SDL_r: // surrender
+							
+						case SDL_b: // bet
+							
+					}
+
+				// check for mouse button press
+				case SDL_MOUSEBUTTONUP:
+					if (event.button.button == SDL_BUTTON_LEFT){
+						// check position to add player
+					}
+
+				// check for quit cross press
+				case SDL_QUIT:
+					quit = 1;
+			}
+		}
+	}
+	
 }
